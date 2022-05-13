3b9d71b5cc6e83c781ad20cc2342940d4632f526
Josh Mathiak <jmathiak4@gmail.com>
2022-05-12 19:35:57 -0500

    Finished all parts

--- functions.js
+++ functions.js
@@ -11,4 +11,79 @@ function capitalize(word) {
   return res;
 }
 
-export default capitalize;
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+
+function caesarCipher(word) {
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let arr = word.split("");
+  for (let i = 0; i < arr.length; i++) {
+    let ind = alphabet.indexOf(arr[i]);
+    if (!arr[i].match(/^[.,:!?]/)) {
+      if (ind == 25) {
+        arr[i] = alphabet[0];
+      } else {
+        arr[i] = alphabet[ind + 1];
+      }
+    }
+  }
+  return arr.join("");
+}
+
+function analyzeArray(array) {
+  let averageNum = array.reduce((prev, cur) => prev + cur, 0) / array.length;
+  let minNum = Math.min(...array);
+  let returnObj = {
+    average: averageNum,
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length,
+  };
+
+  return returnObj;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,52 @@
-import capitalize from "./functions.js";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./functions.js";
 
 test("Capitalize first letter of word", () => {
   expect(capitalize("joshua")).toBe("Joshua");
+});
+
+test("Reverse String", () => {
+  expect(reverseString("Zenyatta")).toBe("attayneZ");
+});
+
+test("Add", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+});
+
+test("Subtract", () => {
+  expect(calculator.subtract(6, 3)).toBe(3);
+});
+
+test("Divide", () => {
+  expect(calculator.divide(15, 3)).toBe(5);
+});
+
+test("Multiply", () => {
+  expect(calculator.multiply(23, 3)).toBe(69);
+});
+
+test("Caesar Cipher: Happy", () => {
+  expect(caesarCipher("inrg")).toBe("josh");
+});
+
+test("Caesar Cipher: Wrapping", () => {
+  expect(caesarCipher("wxyz")).toBe("xyza");
+});
+
+test("Caesar Cipher: Punctuation", () => {
+  expect(caesarCipher("w.x.y.z.")).toBe("x.y.z.a.");
+});
+
+test("Analyze Array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
